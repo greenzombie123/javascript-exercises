ad11e5959a7f07283e988a65f029aa98c2c5fbc1
Alvin Hart <pageking123@gmail.com>
2022-07-14 23:55:13 +0900

    Finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  var sum = 0
+	for (const item of array) {
+    sum += item;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let finalSum = 0;
+	for (let index = 0; index < num.length; index++) {
+    if(index === 0){
+      finalSum = num[index];
+      continue;
+    }
+    finalSum = finalSum * num[index];
+  }
+  
+  return finalSum;
 };
 
-const power = function() {
-	
+const power = function(baseNum, powerNum) {
+	return baseNum ** powerNum
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  let factorial = [];
+  let sum = 1;
+	for (let index = 1; index <= num; index++) {
+    factorial.push(index)
+  }
+  
+  factorial.forEach(num=>sum = sum * num);
+
+  return sum;
 };
 
 // Do not edit below this line
